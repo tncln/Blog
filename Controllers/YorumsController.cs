30157d28a865da31bf87ddf1a4602a36390a5684
Adem Tunçalın <tuncalin@gmail.com>
2018-02-07 19:37:09 +0300

    Version 1.0

---
+++ Controllers/YorumsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using mvcBlog.Models;
+
+namespace mvcBlog.Controllers
+{
+    public class YorumsController : Controller
+    {
+        private mvcblogDB db = new mvcblogDB();
+
+        // GET: Yorums
+        public ActionResult Index()
+        {
+            var yorums = db.Yorums.Include(y => y.Makale).Include(y => y.Uye);
+            return View(yorums.ToList());
+        }
+
+        // GET: Yorums/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Yorum yorum = db.Yorums.Find(id);
+            if (yorum == null)
+            {
+                return HttpNotFound();
+            }
+            return View(yorum);
+        }
+
+        // GET: Yorums/Create
+        public ActionResult Create()
+        {
+            ViewBag.MakaleId = new SelectList(db.Makales, "MakaleId", "Baslik");
+            ViewBag.UyeId = new SelectList(db.Uyes, "UyeId", "KullaniciAdi");
+            return View();
+        }
+
+        // POST: Yorums/Create
+        // Aşırı gönderim saldırılarından korunmak için, lütfen bağlamak istediğiniz belirli özellikleri etkinleştirin, 
+        // daha fazla bilgi için https://go.microsoft.com/fwlink/?LinkId=317598 sayfasına bakın.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "YorumId,Icerik,UyeId,MakaleId,Tarih")] Yorum yorum)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Yorums.Add(yorum);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.MakaleId = new SelectList(db.Makales, "MakaleId", "Baslik", yorum.MakaleId);
+            ViewBag.UyeId = new SelectList(db.Uyes, "UyeId", "KullaniciAdi", yorum.UyeId);
+            return View(yorum);
+        }
+
+        // GET: Yorums/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Yorum yorum = db.Yorums.Find(id);
+            if (yorum == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.MakaleId = new SelectList(db.Makales, "MakaleId", "Baslik", yorum.MakaleId);
+            ViewBag.UyeId = new SelectList(db.Uyes, "UyeId", "KullaniciAdi", yorum.UyeId);
+            return View(yorum);
+        }
+
+        // POST: Yorums/Edit/5
+        // Aşırı gönderim saldırılarından korunmak için, lütfen bağlamak istediğiniz belirli özellikleri etkinleştirin, 
+        // daha fazla bilgi için https://go.microsoft.com/fwlink/?LinkId=317598 sayfasına bakın.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "YorumId,Icerik,UyeId,MakaleId,Tarih")] Yorum yorum)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(yorum).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.MakaleId = new SelectList(db.Makales, "MakaleId", "Baslik", yorum.MakaleId);
+            ViewBag.UyeId = new SelectList(db.Uyes, "UyeId", "KullaniciAdi", yorum.UyeId);
+            return View(yorum);
+        }
+
+        // GET: Yorums/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Yorum yorum = db.Yorums.Find(id);
+            if (yorum == null)
+            {
+                return HttpNotFound();
+            }
+            return View(yorum);
+        }
+
+        // POST: Yorums/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Yorum yorum = db.Yorums.Find(id);
+            db.Yorums.Remove(yorum);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
